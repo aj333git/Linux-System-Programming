5c52253d6ab0f21e826aaaa47fece8a2774600d3
Amit Joshi <113094447+aj333git@users.noreply.github.com>
2025-01-02 15:18:21 +0530

    Add files via upload
    fork()

---
+++ ex2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    pid_t pid;
+ //pid is process id for unique identifier
+    pid = fork();
+    //fork() creates a new process(child process) by duplicating the calling process.
+    if (pid < 0) {
+        printf("failed to fork..\n");
+    } else if (pid == 0) {
+        printf("child process ..\n");
+    } else if (pid > 0) {
+        printf("parent process..\n");
+    }
+    //sleep() causes the calling thread to sleep 
+    sleep(2);
+    return 0;
+}
+
+//parent process..
+//child process ..

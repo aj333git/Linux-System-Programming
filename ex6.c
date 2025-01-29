a02376be175c6d74c2efc230291fe36af86188db
Amit Joshi <113094447+aj333git@users.noreply.github.com>
2025-01-29 16:18:27 +0530

    Update ex6.c

--- ex6.c
+++ ex6.c
@@ -7,8 +7,10 @@
 
 int main(){
 //key_t key = ftok("memory",67);
+  //ftok generates a unique key
 key_t key = ftok("memory", 67);
 //int shmid = shmget(key, 1024,0666|IPC_CREAT);
+  //shmget returns an identifier,after successfull completion
 int shmid = shmget(key, 1024,0666 | IPC_CREAT);
 if(shmid == -1)
 {
@@ -17,6 +19,7 @@ printf("Unable to create the Shared Memory Segment.\n");
 //printf("unable to create the shared Memory Segment.\n");
 //attach shared memory
 //char *str = (char*) shmat(shmid,(void*)0,0);
+  //before using shared memoery region, have to attach it via shmat
 char *str = (char*)shmat(shmid, (void*)0,0);
 printf("enter data to write into shared memory segment");
 scanf("%[^\n] s", str);
